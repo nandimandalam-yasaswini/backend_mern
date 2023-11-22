da00aa512f5583d5adca00199c8344e3fe6e2038
nandimandalam yasaswini <nyasaswini2004@gmail.com>
2023-11-22 12:45:17 +0530

    first commit

--- routes/student_route.js
+++ routes/student_route.js
@@ -52,10 +52,12 @@ router.post("/signin", (req, res) => {
     console.log(req.body)
     const { roll_no, password } = req.body;
     if (!roll_no || !password) {
+        console.log(roll_no);
         return res.status(422).json({ error: "please add roll_no amd password" });
     }
     Student.findOne({ roll_no: roll_no }).then((savedUser) => {
         if (!savedUser) {
+            console.log(roll_no);
             return res.status(422).json({ error: "Invalid roll_no or password" });
         }
         bcrypt
@@ -70,13 +72,16 @@ router.post("/signin", (req, res) => {
                         user: savedUser
                     });
                 } else {
+                    console.log(roll_no);
                     return res.status(422).json({ error: "Invalid roll_no or password" });
                 }
             })
             .catch((err) => {
                 console.log(err);
+                console.log(roll_no);
             });
     });
+    console.log(roll_no);
 });
 
 router.get("/profile", requireLogin, (req, res) => {
--- server.js
+++ server.js
@@ -37,11 +37,12 @@ app.use('/api/issues', require("./routes/issue_route"))
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static('client/build'))
 
-    app.get('*', (req, res) => {
+   app.get('*', (req, res) => {
 
         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
 
     })
+
 }
 
 
